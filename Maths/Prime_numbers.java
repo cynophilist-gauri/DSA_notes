b507583ba3143e7ee350c80d1cfeac2b85b4d023
gauri_agarwal <gauriagarwal1001@gmail.com>
2023-04-02 01:13:00 +0530

    Main

--- Maths/Prime_numbers.java
+++ Maths/Prime_numbers.java
@@ -1,4 +1,18 @@
-import java.io.*
+/*
+ Prime numbers: 2,3,5,7,13....
+ (divisible by itself and 1)
+
+ ex. Check for no.36
+     1*36 <--
+     2*18 <--              
+     3*12 <--
+     4*9 <--
+     6*6
+ --> 9*4                       *Getting repeated, since(2*18 = 18*2)
+ --> 12*3                      *Hence only check for <= sqrt(n)
+ --> 18*2                      *Compexity also changes from O(n) -> O(√n)
+ --> 36*1
+ */
 
 public class Prime{
 public static void main(String args[]){
