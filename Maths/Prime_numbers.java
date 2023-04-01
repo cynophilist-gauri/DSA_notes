6d33117c3413b987764cf410959d4698adae4af4
gauri_agarwal <gauriagarwal1001@gmail.com>
2023-04-01 19:01:35 +0530

    Main

---
+++ Maths/Prime_numbers.java
@@ -0,0 +1,21 @@
+import java.io.*
+
+public class Prime{
+public static void main(String args[]){
+
+    int n = 20;
+    isPrime(n);
+
+}
+}
+    static boolean isPrime(int n){
+        if(n <= 1){
+          return false;
+        } 
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n%i == 0){
+              return false;
+            }
+            }
+        return true;
+    }
