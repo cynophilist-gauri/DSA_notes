4fce885444a56f1df7760e671a68019696c233f3
gauri_agarwal <gauriagarwal1001@gmail.com>
2023-04-05 01:02:52 +0530

    Main

--- ArrayList/SortArrayList+other_functions.java
+++ ArrayList/SortArrayList+other_functions.java
@@ -4,12 +4,21 @@ class SortArrayList{
         //Creating a list of numbers
         ArrayList<Integer> sorted_array=new ArrayList<Integer>();
         sorted_array.add(10);
-        sorted_array.add(20);
+        sorted_array.add(40);
         sorted_array.add(30);
         sorted_array.add(40);
 
         //Sorting the list
-        Collections.sort(sorted_array);
+        Collections.sort(sorted_array); // O(nlog(n))
+
+        //Other functions
+        Collections.frequency(40);
+        Collections.swap(sorted_array,2,3);
+        Collections.reverseOrder(sorted_array);
+        obj.get(1);
+        obj.remove(3);
+        obj.set(1,90);
+        obj.size();
 
         //Traversing list through the for-each loop
         for(Integer number:sorted_array{
